15b2a718a00821493a3a0d8307d6ed2bc338f200
kindsenior <kunio.einstein@gmail.com>
2025-07-20 11:12:35 +0900

    add color config in modeling_robot_configuration.py

--- modeling_robot_configuration.py
+++ modeling_robot_configuration.py
@@ -5,6 +5,10 @@ import numpy as np
 import pinocchio as pin
 from robot_wrapper import *
 
+red   = [1,0,0, 0.7]
+green = [0,1,0, 0.7]
+blue  = [0,0,1, 0.7]
+
 # generate robot model
 model = pin.Model()
 visual_model = pin.GeometryModel()
@@ -24,21 +28,21 @@ root_joint_id = joint_id
 
 # rleg
 parent_joint_id = root_joint_id
-parent_joint_id, _ = robot.add_joint_link("rleg_joint1", "rleg_link1", [0,0,1],  [0,-0.1,-0.1], parent_joint_id, color=[1,0,0, 1])
-parent_joint_id, _ = robot.add_joint_link("rleg_joint2", "rleg_link2", [1,0,0],  [0,0,-0.1],    parent_joint_id, color=[0,1,0, 1])
-parent_joint_id, _ = robot.add_joint_link("rleg_joint3", "rleg_link3", [0,1,1],  [0,0,0],       parent_joint_id, link_translation=[0,0,-0.5], color=[0,0,1, 1])
-parent_joint_id, _ = robot.add_joint_link("rleg_joint4", "rleg_link4", [0,1,0],  [0,0,-0.5],    parent_joint_id, link_translation=[0,0,-0.5], color=[1,0,0, 1])
-parent_joint_id, _ = robot.add_joint_link("rleg_joint5", "rleg_link5", [0,1,0],  [0,0,-0.5],    parent_joint_id, color=[0,1,0, 1])
-parent_joint_id, _ = robot.add_joint_link("rleg_joint6", "rleg_link6", [1,0,0],  [0,0,0],       parent_joint_id, link_translation=[0.15,0,0], color=[0,0,1, 1])
+parent_joint_id, _ = robot.add_joint_link("rleg_joint1", "rleg_link1", [0,0,1],  [0,-0.1,-0.1], parent_joint_id, color=red)
+parent_joint_id, _ = robot.add_joint_link("rleg_joint2", "rleg_link2", [1,0,0],  [0,0,-0.1],    parent_joint_id, color=green)
+parent_joint_id, _ = robot.add_joint_link("rleg_joint3", "rleg_link3", [0,1,1],  [0,0,0],       parent_joint_id, link_translation=[0,0,-0.5], color=blue)
+parent_joint_id, _ = robot.add_joint_link("rleg_joint4", "rleg_link4", [0,1,0],  [0,0,-0.5],    parent_joint_id, link_translation=[0,0,-0.5], color=red)
+parent_joint_id, _ = robot.add_joint_link("rleg_joint5", "rleg_link5", [0,1,0],  [0,0,-0.5],    parent_joint_id, color=green)
+parent_joint_id, _ = robot.add_joint_link("rleg_joint6", "rleg_link6", [1,0,0],  [0,0,0],       parent_joint_id, link_translation=[0.15,0,0], color=blue)
 
 # lleg
 parent_joint_id = root_joint_id
-parent_joint_id, _ = robot.add_joint_link("lleg_joint1", "lleg_link1", [0,0,1],  [0,0.1,-0.1],  parent_joint_id, color=[1,0,0, 1])
-parent_joint_id, _ = robot.add_joint_link("lleg_joint2", "lleg_link2", [1,0,0],  [0,0,-0.1], parent_joint_id, color=[0,1,0, 1])
-parent_joint_id, _ = robot.add_joint_link("lleg_joint3", "lleg_link3", [0,-1,1], [0,0,0],       parent_joint_id, link_translation=[0,0,-0.5], color=[0,0,1, 1])
-parent_joint_id, _ = robot.add_joint_link("lleg_joint4", "lleg_link4", [0,1,0],  [0,0,-0.5],    parent_joint_id, link_translation=[0,0,-0.5], color=[1,0,0, 1])
-parent_joint_id, _ = robot.add_joint_link("lleg_joint5", "lleg_link5", [0,1,0],  [0,0,-0.5],    parent_joint_id, color=[0,1,0, 1])
-parent_joint_id, _ = robot.add_joint_link("lleg_joint6", "lleg_link6", [1,0,0],  [0,0,0],       parent_joint_id, link_translation=[0.15,0,0], color=[0,0,1, 1])
+parent_joint_id, _ = robot.add_joint_link("lleg_joint1", "lleg_link1", [0,0,1],  [0,0.1,-0.1],  parent_joint_id, color=red)
+parent_joint_id, _ = robot.add_joint_link("lleg_joint2", "lleg_link2", [1,0,0],  [0,0,-0.1],    parent_joint_id, color=green)
+parent_joint_id, _ = robot.add_joint_link("lleg_joint3", "lleg_link3", [0,-1,1], [0,0,0],       parent_joint_id, link_translation=[0,0,-0.5], color=blue)
+parent_joint_id, _ = robot.add_joint_link("lleg_joint4", "lleg_link4", [0,1,0],  [0,0,-0.5],    parent_joint_id, link_translation=[0,0,-0.5], color=red)
+parent_joint_id, _ = robot.add_joint_link("lleg_joint5", "lleg_link5", [0,1,0],  [0,0,-0.5],    parent_joint_id, color=green)
+parent_joint_id, _ = robot.add_joint_link("lleg_joint6", "lleg_link6", [1,0,0],  [0,0,0],       parent_joint_id, link_translation=[0.15,0,0], color=blue)
 
 # initialize data
 robot.q0 = np.zeros(robot.model.nq, dtype=np.double)
